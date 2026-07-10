e0b412a99a3f21d614f5ff168443508b03c06aeb
HamUa <95013457+HamUa@users.noreply.github.com>
2026-07-10 19:50:53 +0300

    hw_3_2

---
+++ lesson_3/hw_3_2.py
@@ -0,0 +1,31 @@
+ numbers = [12, 3, 4, 10]
+
+if len(numbers) > 1:
+    numbers.insert(0, numbers[-1])
+    numbers.pop()
+
+print(numbers)
+
+# numbers = [1]
+#
+# if len(numbers) > 1:
+#     numbers.insert(0, numbers[-1])
+#     numbers.pop()
+#
+# print(numbers)
+#
+# numbers = []
+#
+# if len(numbers) > 1:
+#     numbers.insert(0, numbers[-1])
+#     numbers.pop()
+#
+# print(numbers)
+
+# numbers = [12, 3, 4, 10, 8]
+#
+# if len(numbers) > 1:
+#     numbers.insert(0, numbers[-1])
+#     numbers.pop()
+#
+# print(numbers)
