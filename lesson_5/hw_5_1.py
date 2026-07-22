46fbae507f4dda4f3b81e1ce9277e7cd83745fbc
HamUa <95013457+HamUa@users.noreply.github.com>
2026-07-22 23:40:40 +0300

    Remove variable name validation code
    hw_5.1

--- lesson_5/hw_5_1.py
+++ lesson_5/hw_5_1.py
@@ -31,36 +31,7 @@ if __name__ == "__main__":
     print(is_valid_variable_name(variable_name))
 
 
-user_input = input("Введіть назву змінної: ")
 
-allowed_punctuation = "_"
-forbidden_punctuation = set(string.punctuation) - set(allowed_punctuation)
-
-is_valid = True
-if user_input[0].isdigit():
-    is_valid = False
-elif any(char.isupper() for char in user_input):
-    is_valid = False
-elif any(char.isspace() or char in forbidden_punctuation for char in user_input):
-    is_valid = False
-elif "__" in user_input:
-    is_valid = False
-elif user_input in keyword.kwlist:
-    is_valid = False
-
-print(is_valid)
-
-# _ => True
-# __ => False
-# ___ => False
-# x => True
-# get_value => True
-# get value => False
-# get!value => False
-# some_super_puper_value => True
-# Get_value => False
-# get_Value => False
-# getValue => False
 # 3m => False
 # m3 => True
 # assert => False
